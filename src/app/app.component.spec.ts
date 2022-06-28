61f0a7d35526f71dd8897df6422e980553c044de
Pierre Gaimard <pierre@gaimard-family.fr>
2022-06-28 21:34:41 +0200

    Change component title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'test-3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-3');
+    expect(app.title).toEqual('test-32');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('test-3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('test-32 app is running!');
   });
 });
